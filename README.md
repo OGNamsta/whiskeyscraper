7a6150e7fa36ebb0abeafbcd0fbc4b70afa85ee1
Tengeevandu <64324719+OGNamsta@users.noreply.github.com>
2023-12-29 19:58:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# whiskeyscraper
+ This scraper was built using Scrapy. It extracts a list of products from www.whiskyshop.com
---
+++ whiskeyscraper/spiders/whiskeyspider.py
@@ -0,0 +1,23 @@
+import scrapy
+from whiskeyscraper.items import WhiskeyScraperItem
+from scrapy.loader import ItemLoader
+
+
+class WhiskeySpider(scrapy.Spider):
+    name = 'whiskey'
+    start_urls = ['https://www.whiskyshop.com/scotch-whisky/all?item_availability=In+Stock']
+
+    def parse(self, response):
+        for products in response.css('div.product-item-info'):
+            loader = ItemLoader(item=WhiskeyScraperItem(), selector=products)
+
+            loader.add_css('name', 'a.product-item-link')
+            loader.add_css('price', 'span.price')
+            loader.add_css('link', 'a.product-item-link::attr(href)')
+
+            yield loader.load_item()
+
+        next_page = response.css('a.action.next').attrib['href']
+        if next_page is not None:
+            # yield response follow
+            yield response.follow(next_page, callback=self.parse)
