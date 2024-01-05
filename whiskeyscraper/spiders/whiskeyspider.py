4b01941e48765277cce1aff25c87ef2bd7c29d5c
Tengeevandu <64324719+OGNamsta@users.noreply.github.com>
2024-01-05 20:12:12 +0200

    When the products are out of stock, the price area is empty. So we'll just place N/A in its place.

--- whiskeyscraper/spiders/whiskeyspider.py
+++ whiskeyscraper/spiders/whiskeyspider.py
@@ -9,13 +9,13 @@ class WhiskeySpider(scrapy.Spider):
 
     def parse(self, response):
         for products in response.css('div.product-item-info'):
-            loader = ItemLoader(item=WhiskeyScraperItem(), selector=products)
+                loader = ItemLoader(item=WhiskeyScraperItem(), selector=products)
 
-            loader.add_css('name', 'a.product-item-link')
-            loader.add_css('price', 'span.price')
-            loader.add_css('link', 'a.product-item-link::attr(href)')
+                loader.add_css('name', 'a.product-item-link')
+                loader.add_css('price', 'span.price', default='N/A')
+                loader.add_css('link', 'a.product-item-link::attr(href)')
 
-            yield loader.load_item()
+                yield loader.load_item()
 
         next_page = response.css('a.action.next').attrib['href']
         if next_page is not None:
